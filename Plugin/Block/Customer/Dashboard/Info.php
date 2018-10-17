67b5bbaa37db946caf6543183c292dacdc3729ce
Mathis Klooß <mathis@mage-profis.de>
2018-10-17 15:15:43 +0200

    fixed issues with composer vendor

--- Plugin/Block/Customer/Dashboard/Info.php
+++ Plugin/Block/Customer/Dashboard/Info.php
@@ -3,7 +3,7 @@
 namespace Loewenstark\Maileon2\Plugin\Block\Customer\Dashboard;
 
 use Magento\Customer\Block\Account\Dashboard\Info as DashboardInfo;
-use Loewenstark\Maileon2\Plugin\Block\Customer\Dashboard as CustomerDashboard;
+use Loewenstark\Maileon2\Plugin\Block\Customer\AbstractPlugin as CustomerDashboard;
 
 class Info extends CustomerDashboard
 {
--- Plugin/Block/Customer/Newsletter.php
+++ Plugin/Block/Customer/Newsletter.php
@@ -4,7 +4,7 @@ namespace Loewenstark\Maileon2\Plugin\Block\Customer;
 
 use \Magento\Customer\Block\Newsletter as CustomerNewsletter;
 
-class Newsletter extends Dashboard
+class Newsletter extends AbstractPlugin
 {
     public function aroundGetIsSubscribed(CustomerNewsletter $model, callable $proceed)
     {
